e6d0156d5021e6a89d8c070a422dcbabf456cbb4
Krutikov Nikita <krutikov.nikita7@gmail.com>
2021-10-31 01:55:21 +0700

    Более богатый функционал лотереи, доработка тестов

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,60 +3,153 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+/**
+ * @title Контракт простой одноразовой лотереи.
+ * Администратор запускает лотерею. Пользователи вносят эфир.
+ * Через некоторое заданное администратором время администратор запускает розыгрыш, один из счетов получает выигрыш, остальные деньги перечисляются администратору, контракт ликвидируется.
+ */
 contract Lottery {
-    address public manager;
-    address[] public players;
+    // Длительность лотереи
     uint public lotteryDuration;
+
+    // Стоимость билета
     uint public ticketPrice;
+
+    // Размер приза
     uint public prizeAmount;
+
+    // Дата начала лотереи
     uint public start;
 
+    // Дата конца лотереи
+    uint public end;
+
+    // Адрес менеджера. 
+    address private manager;
+
+    // Список адресов игроков.
+    address[] private players;
+
+
+    event LotteryTicketPurchased(address player);
+    event LotteryFinish(address winner);
+    // event 
+
+    /**
+     * @notice Модификатор, проверка, что функции под этим модификатормо может выполнять только мэнеджер лотереи.
+     */
     modifier managerOnly() {
         require(msg.sender == manager, "manager Only");
         _;
     }
 
+    /**
+     * @notice Конструктор лотереи. Кто делоит контракт, тот становится администратором.
+     *
+     * @param _lotteryDuration Длительность лотереи в днях.
+     * @param _ticketPrice Стоимость билета в wei.
+     * @param _prizeAmount Размер приза в лотерее в wei.
+     */
     constructor(uint _lotteryDuration, uint _ticketPrice, uint _prizeAmount) {
         manager = msg.sender;
         lotteryDuration = _lotteryDuration;
         ticketPrice = _ticketPrice;
         prizeAmount = _prizeAmount;
         start = block.timestamp;
+        end = start + _lotteryDuration * 1 days;
 
-        console.log("Deploying a Lottery:");
-        console.log("manager:", manager);
-        console.log("lotteryDuration:", _lotteryDuration);
-        console.log("ticketPrice:", _ticketPrice);
-        console.log("prizeAmount:", _prizeAmount);
-        console.log("start:", start);
+        // console.log("\t %cDeploying a Lottery:", "font-style: bold");
+        // console.log("\t   manager:", manager);
+        // console.log("\t   lotteryDuration:", _lotteryDuration);
+        // console.log("\t   ticketPrice:", _ticketPrice);
+        // console.log("\t   prizeAmount:", _prizeAmount);
+        // console.log("\t   start:", start);
+        // console.log("\t   end:", end);
     }
 
+    /**
+     * @notice Покупка лотерейного билета при помощи вызова функции.
+     */
     function buyLotteryTicket() public payable {
-        console.log("try buy ticket for ", msg.value);
-        require(msg.value >= ticketPrice, "Price is higher");
+        // console.log("Try buy ticket for ", msg.value, msg.sender);
+        require(msg.value == ticketPrice, "Price is wrong");
+        require(!isEnded(), "Lottery is over");
 
-        console.log("Contract's money is ", address(this).balance);
-        console.log("%s is added to players", msg.sender);
+        // console.log("Contract's money is ", address(this).balance);
         players.push(msg.sender);
+        // console.log("%s is added to players/ Player's count is %s", msg.sender, players.length);
+        emit LotteryTicketPurchased(msg.sender);
     }
 
-    function finishLottery() public managerOnly returns (address payable winnerAddress) {
-        console.log("try finishLottery");
-        // require(block.timestamp >= start + lotteryDuration * 1 days, "is not over yet");
-        require(address(this).balance > prizeAmount, "doesn't have enough money in the bank");
-
-        uint winnerIndex = pseudoRandom() % players.length;
-        winnerAddress = payable(players[winnerIndex]);
-        winnerAddress.transfer(prizeAmount);
-        console.log("winner is %s %s", winnerAddress, address(winnerAddress).balance);
-        players = new address[](0);
+    /**
+     * @notice Покупка лотерейного билета просто при помощи кидания денег на адрес лотереи.
+     */
+    receive() external payable {
+        buyLotteryTicket();
     }
 
+    /**
+     * @notice Закончить лотерею. 
+     */
+    function finishLottery() public managerOnly returns (address) {
+        // console.log("try finishLottery");
+        require(block.timestamp >= end, "is not over yet");
+
+        // На счету лотереи должно быть чуть больше денег, чем для выирыша, потому что нужно заплатить за транзацию.
+        // Я не знаю, сколько стоит транзация, поэтому считаю, что в среднем тратится 2300 gas (есть мнение что 21000)
+        // Альтернативный вариант - пусть победитель сам оплачивает транзакцию, то есть из выигрыша вычесть сумму за газ.
+        require(address(this).balance > (prizeAmount + tx.gasprice*2300), "doesn't have enough money in the bank");
+
+        address payable winnerAddress;
+
+        // На счету до начала лотереи могут быть уже деньги, поэтому проверка на баланс может не сработать.
+        if (players.length > 0) {
+            uint winnerIndex = pseudoRandom() % players.length;
+            winnerAddress = payable(players[winnerIndex]);
+            winnerAddress.transfer(prizeAmount);
+            emit LotteryFinish(winnerAddress);
+            console.log("winner is %s %s", winnerAddress, address(winnerAddress).balance);
+            players = new address[](0); // Если лотерея одноразовая, строчка не нужна. Если буду переделывать на многоразовую, то оставить
+        }
+
+        /**
+         * После выплаты выигрыша лотереи победителю, остаток переводим менеджеру.
+         */
+        selfdestruct(payable(manager));
+        return winnerAddress;
+    }
+
+    /**
+     * @notice Получить псевдо-рандомное число.
+     * @dev Этот метод не рекомендуется использовать в релизе.
+     */
     function pseudoRandom() private view returns (uint) {
         return uint(
             keccak256(
                 abi.encodePacked(block.difficulty, block.timestamp, players)
             )
         );
+    }
+
+    /**
+     * @notice Способ узнать, является ли вызывающий эту функцию менеджером
+     */
+    function isManager() public view returns (bool) {
+        return msg.sender == manager;
+    }
+
+    /**
+     * @notice Получить список адресов участников.
+     * @dev Не знаю, можно ли разглашать эту информацию, но пусть будет.
+     */
+    function getPlayers() public view returns (address[] memory) {
+        return players;
+    }
+
+    /**
+     * @notice Узнать, закончилась ли уже лотерея.
+     */
+    function isEnded() public view returns (bool) {
+        return block.timestamp >= end;
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,97 +1,227 @@
-import { equal } from "assert";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { addAbortSignal } from "stream";
 import { Lottery } from "../typechain";
-import { Web3Provider } from "@ethersproject/providers";
-import {BigNumber} from "ethers";
+import { ContractTransaction } from "ethers";
 
-describe("Lottery", function () {
-  const lotteryDuration = 1;
-  //                                    45500804539494 - average cost of empty transaction
-  //                                  1129987031480624 - average cost of transaction
-  const ticketPrice = BigNumber.from("1000000000000000");   
-  const prizeAmount = BigNumber.from("6900000000000000");  
+const { time } = require('@openzeppelin/test-helpers');
 
-  // const ticketPrice = BigNumber.from("100");   
-  // const prizeAmount = BigNumber.from("590"); 
 
-  let LotteryFactory;
+describe("Lottery", function() {
+  const lotteryDuration = 1; //days
+  //                                    40000000000000 - average cost of empty transaction
+  //                                   130000000000000 - average cost of transaction
+  const ticketPrice = ethers.utils.parseEther("0.01");
+  const prizeAmount = ethers.utils.parseEther("0.12");  
   let lottery: Lottery;
-  let owner: SignerWithAddress;
-  let addr1: SignerWithAddress;
-  let addr2: SignerWithAddress;
-  let addr3: SignerWithAddress;
-  let addr4: SignerWithAddress;
-  let addr5: SignerWithAddress;
-  let addr6: SignerWithAddress;
   let addrs: SignerWithAddress[];
+  let [manager, addr1, addr2, addr3, addr4, addr5, addr6, ...addr]: SignerWithAddress[] = [];
+  let lotteryAddress: SignerWithAddress;
+
 
   this.beforeAll(async function () {
-    LotteryFactory = await ethers.getContractFactory("Lottery");
-    [owner, addr1, addr2, addr3, addr4, addr5, addr6, ...addrs] = await ethers.getSigners();
-
+    const LotteryFactory = await ethers.getContractFactory("Lottery");
+    [manager, addr1, addr2, addr3, addr4, addr5, addr6, ...addr] = await ethers.getSigners();
     lottery = await LotteryFactory.deploy(lotteryDuration, ticketPrice, prizeAmount);
     await lottery.deployed();
+    lotteryAddress = await ethers.getSigner(lottery.address);
+    console.log("\t%clottery.address: %s", "font-style: bold", lottery.address);
+    // const balance = await ethers.provider.getBalance(lottery.address);
+    // console.log("lottery balance: ", balance.toString());
   });
 
-  describe("Deployment", function() {
-    it("Should set the right owner", async function () {
-      equal(await lottery.manager(), owner.address);
-      // expect(await lottery.manager()).to.equal(owner.address);
-    });
-
-    it("Should set the right lotteryDuration", async function () {
-      expect(await lottery.lotteryDuration()).to.equal(lotteryDuration);
-    });
-
-    it("Should set the right ticketPrice", async function () {
-      expect(await lottery.ticketPrice()).to.equal(ticketPrice);
-    });
-
-    it("Should set the right prizeAmount", async function () {
-      expect(await lottery.prizeAmount()).to.equal(prizeAmount);
-    });
+  describe("Деплой", function() {
+    it("Инициализация лотереи", async function () {
+      expect(await lottery.isManager()).to.equal(true, "Менеджер установлен не верно");
+      expect(await lottery.lotteryDuration()).to.equal(lotteryDuration, "Длительность лотереи установлена не верно");
+      expect(await lottery.ticketPrice()).to.equal(ticketPrice, "Стоимость билета установлена не верно");
+      expect(await lottery.prizeAmount()).to.equal(prizeAmount, "Размер приза установлен не верно");
+      const players = await lottery.getPlayers();
+      expect(players.length).to.equal(0, "Список участников в начале лотереи не пуст");
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+    })
   });
 
-  describe("Transactions: Buying lotteryTicket", function() {
-    it("HappyPath", async function () {
-      const initialAddr1Balance = await addr1.getBalance();
-      await lottery.connect(addr1).buyLotteryTicket({value: ticketPrice});
-      // expect(await addr1.getBalance()).to.equal(initialAddr1Balance.sub(ticketPrice));
-      const endBalance = await addr1.getBalance();
-      const diff = initialAddr1Balance.sub(endBalance);
-      console.log("Buy %s - %s = %s", initialAddr1Balance.toString(), endBalance.toString(), diff.toString());
-      console.log("players + ", await lottery.players(0));
-    });
-    
-    it("Should fail if sender doesn’t send enough wei", async function () {
-      const initialAddr1Balance = await addr1.getBalance();
-      
+  describe("Покупка лотерейного билета", function() {
+    it("HappyPath, покупка через функцию 'buyLotteryTicket'", async function () {
+      // Смысла проверять, не закончилась ли лотерея не очень много, но пусть будет.
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      let players = await lottery.getPlayers();
+      expect(players.length).to.equal(0, "Список участников в начале лотереи не пуст");
+
       // const initialAddr1Balance = await addr1.getBalance();
-      await expect(
-        lottery.connect(addr1).buyLotteryTicket({value: ticketPrice.sub(1)})
-      ).to.be.revertedWith("Price is higher");
-      const endBalance = await addr1.getBalance();
-      const diff = initialAddr1Balance.sub(endBalance);
-      console.log("Not Buy %s - %s = %s", initialAddr1Balance.toString(), endBalance.toString(), diff.toString())
-      console.log("players - ", await lottery.players(0));
+      await expect(await lottery.connect(addr1).buyLotteryTicket({value: ticketPrice}), "Лотерея получила отличное от цены билета количество эфира").to.changeEtherBalance(lotteryAddress, ticketPrice);
+
+      // const endBalance = await addr1.getBalance();
+      // const diff = initialAddr1Balance.sub(endBalance);
+      // const tax = diff.sub(ticketPrice);
+      // console.log("Buy %s - %s = %s, tax = %s", ethers.utils.formatEther(initialAddr1Balance), ethers.utils.formatEther(endBalance), ethers.utils.formatEther(diff), ethers.utils.formatEther(tax));
+
+      expect(await lottery.isEnded(), "Лотерея неожидано кончилась").to.equal(false);
+      players = await lottery.getPlayers();
+      expect(players.length).to.equal(1, "Количество участников лотереи не верно");
+      expect(players[0]).to.equal(await addr1.getAddress(), "В списке игроков неожиданный участник");
+
+      await expect(await lottery.connect(addr1).buyLotteryTicket({value: ticketPrice}), "С игрока снято отличное от цены билета количество эфира").to.changeEtherBalance(addr1, ticketPrice.mul("-1"));
     });
+
+    it("HappyPath, покупка через посылку денег на адрес контракта", async function () {
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      let players = await lottery.getPlayers();
+      expect(players.length).to.equal(2, "Количество участников лотереи не верно");
+
+      // const initialAddr2Balance = await addr2.getBalance();
+      await expect(await addr2.sendTransaction({to: lottery.address, value: ticketPrice}), "Лотерея получила отличное от цены билета количество эфира").to.changeEtherBalance(lotteryAddress, ticketPrice);
+
+      // const endBalance = await addr2.getBalance();
+      // const diff = initialAddr2Balance.sub(endBalance);
+      // const tax = diff.sub(ticketPrice);
+      // console.log("Buy %s - %s = %s, tax = %s", initialAddr2Balance.toString(), endBalance.toString(), diff.toString(), tax.toString());
+
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      players = await lottery.getPlayers();
+      expect(players.length).to.equal(3, "Количество участников лотереи не верно");
+      expect(players[2]).to.equal(await addr2.getAddress());
+
+      await expect(await addr2.sendTransaction({to: lottery.address, value: ticketPrice}), "С игрока снято отличное от цены билета количество эфира").to.changeEtherBalance(addr2, ticketPrice.mul("-1"));
+    });
+
+    
+    it("Должен упасть, если покупатель отправит недостаточную для билета сумму через функцию 'buyLotteryTicket'", async function () {
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      let players = await lottery.getPlayers();
+      expect(players.length).to.equal(4, "Количество участников лотереи не верно");
+
+      // const initialAddr3Balance = await addr3.getBalance();
+      
+      await expect(
+        lottery.connect(addr3).buyLotteryTicket({value: ticketPrice.sub(1)})
+      ).to.be.revertedWith("Price is wrong");
+      // const endBalance = await addr3.getBalance();
+      // const diff = initialAddr3Balance.sub(endBalance);
+      // console.log("Not Buy %s - %s = %s", initialAddr3Balance.toString(), endBalance.toString(), diff.toString());
+
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      players = await lottery.getPlayers();
+      expect(players.length).to.equal(4, "Количество участников лотереи не верно");
+    });
+
+    it("Должен упасть, если покупатель отправит недостаточную для билета сумму через receive", async function () {
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      let players = await lottery.getPlayers();
+      expect(players.length).to.equal(4, "Количество участников лотереи не верно");
+
+      // const initialAddr4Balance = await addr3.getBalance();
+      
+      expect(
+        addr4.sendTransaction({to: lottery.address, value: ticketPrice.sub(1)})
+      ).to.be.revertedWith("Price is wrong");
+      // const endBalance = await addr4.getBalance();
+      // const diff = initialAddr4Balance.sub(endBalance);
+      // console.log("Not Buy %s - %s = %s", initialAddr4Balance.toString(), endBalance.toString(), diff.toString());
+
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      players = await lottery.getPlayers();
+      expect(players.length).to.equal(4, "Количество участников лотереи не верно");
+    });
+
+    it("Проверка isEnded до и после конца лотереи", async function () {
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      await time.increase(time.duration.days(lotteryDuration));
+      expect(await lottery.isEnded()).to.equal(true, "Время ещё не кончилось");
+    })
   });
 
-  describe("finishLottery", function() {
-    it("finishLottery", async function () {
-      await Promise.all([
-        lottery.connect(addr1).buyLotteryTicket({value: ticketPrice}),
-        lottery.connect(addr2).buyLotteryTicket({value: ticketPrice}),
-        lottery.connect(addr3).buyLotteryTicket({value: ticketPrice}),
-        lottery.connect(addr4).buyLotteryTicket({value: ticketPrice}),
-        lottery.connect(addr5).buyLotteryTicket({value: ticketPrice}),
-        lottery.connect(addr6).buyLotteryTicket({value: ticketPrice})
-      ]);
+  describe("Розыгрыш лотереи", function() {
 
-      const winnerAddress = await lottery.finishLottery();
+    this.beforeEach(async function () {
+      const LotteryFactory = await ethers.getContractFactory("Lottery");
+      [manager, ...addr] = await ethers.getSigners();
+      lottery = await LotteryFactory.deploy(lotteryDuration, ticketPrice, prizeAmount);
+      await lottery.deployed();
     });
+
+    it("HappyPath", async function () {
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      let players = await lottery.getPlayers();
+      expect(players.length).to.equal(0, "Список участников в начале лотереи не пуст");
+
+      // const initialManagerBalance = await manager.getBalance();
+
+      const contractTransactions: Promise<ContractTransaction>[] = [];
+
+      for (const account of addr) {
+        contractTransactions.push(lottery.connect(account).buyLotteryTicket({value: ticketPrice}));
+      }
+
+      await Promise.all(contractTransactions);
+      
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      players = await lottery.getPlayers();
+      expect(players.length).to.equal(addr.length, "Количество участников лотереи не верно");
+
+      await time.increase(time.duration.days(lotteryDuration));
+
+      expect(await lottery.isEnded()).to.equal(true, "Время ещё не кончилось");
+      await lottery.finishLottery();
+      const endLotteryBalance = await ethers.provider.getBalance(lottery.address);
+
+      expect(endLotteryBalance.toString()).to.equal('0', "Баланс лотереи не обнулился");
+
+      expect(lottery.isEnded(), "Контракт лотереи не был удалён").to.be.reverted;
+
+      // console.log("WinnerAddress: ", winnerAddress);
+      // TODO Проверить, что баланс победителя увеличился
+      // const managerEndBalance = await manager.getBalance();
+      // const diff = managerEndBalance.sub(initialManagerBalance);
+      // const endWinnerBalance = await ethers.getDefaultProvider().getBalance(winnerAddress);
+      // console.log("Finish lottery. manager %s - %s = %s, lottery %s", ethers.utils.formatEther(managerEndBalance), ethers.utils.formatEther(initialManagerBalance), ethers.utils.formatEther(diff), endLotteryBalance.toString());
+    });
+
+    it("Окончание лотереи при недоборе суммы выигрыша", async function () {
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      let players = await lottery.getPlayers();
+      expect(players.length).to.equal(0, "Список участников в начале лотереи не пуст");
+
+      await lottery.connect(addr1).buyLotteryTicket({value: ticketPrice});
+      await lottery.connect(addr2).buyLotteryTicket({value: ticketPrice});
+      await lottery.connect(addr3).buyLotteryTicket({value: ticketPrice});
+      
+      expect(await lottery.isEnded()).to.equal(false);
+      players = await lottery.getPlayers();
+      expect(players.length).to.equal(3);
+
+      await time.increase(time.duration.days(lotteryDuration));
+
+      expect(await lottery.isEnded()).to.equal(true);
+      
+      await expect(
+        lottery.finishLottery(),
+        "Не сработала проверка на размер выигрыша"
+      ).to.be.revertedWith("doesn't have enough money in the bank");
+    });
+
+    it("Должен упасть, если менеджер попытается разыграть лотерею до её окончания", async function() {
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      let players = await lottery.getPlayers();
+      expect(players.length).to.equal(0, "Список участников в начале лотереи не пуст");
+
+      const contractTransactions: Promise<ContractTransaction>[] = [];
+
+      for (const account of addr) {
+        contractTransactions.push(lottery.connect(account).buyLotteryTicket({value: ticketPrice}));
+      }
+
+      await Promise.all(contractTransactions);
+      
+      expect(await lottery.isEnded()).to.equal(false, "Лотерея неожидано кончилась");
+      players = await lottery.getPlayers();
+      expect(players.length).to.equal(addr.length, "Количество участников лотереи не верно");
+      
+      await expect(
+        lottery.finishLottery(),
+        "Можно разыграть не оконченную лотерею"
+      ).to.be.revertedWith("is not over yet");
+    })
   })
 });
